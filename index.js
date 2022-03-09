97e51a7e43e312109a5813b33a91b3e4a6673f1c
flowsinator~ <florezalbeiro87@gmail.com>
2022-03-09 17:59:34 -0500

    not my best final version at all, need to complete

---
+++ index.js
@@ -0,0 +1,9 @@
+let firstAnswer = window.prompt('Hello friend! It is time to chose a path, should you go left or right?')
+if(firstAnswer === 'left'){
+    let secondAnswer = window.promt('Shhhh, quite, it seems there is a bear sleeping ahead. Do you want to skeak past it, keep walking?')
+    
+} else if(firstAnswer === 'right'){
+    let secondAnswer = window.prompt('WOW It IS SOOO LOUD IN HERE... A bunch of kids from the nearby college are having a rager, you can either sit and have a cold one, or keep walking passed them to that shine chest at the end of the tunnel, which one do you choose?')
+
+}
+console.log(answer === 'left')
